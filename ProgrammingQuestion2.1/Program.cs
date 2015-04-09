f6eb59bdcaa096f0084c0f8e2f35cb36527ddb43
Federico Javier Lopez Bertoni <federico@greenlight.com.py>
2015-04-09 10:55:36 -0400

    Read data

--- ProgrammingQuestion2.1/Program.cs
+++ ProgrammingQuestion2.1/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SharedUtilities;
 
 namespace ProgrammingQuestion2._1
 {
@@ -11,5 +13,19 @@ namespace ProgrammingQuestion2._1
         static void Main(string[] args)
         {
         }
+
+        private static List<EdgeCosts> ReadData(string filename)
+        {
+            var txtData = File.ReadLines(filename).ToArray();
+            var inputData = new List<EdgeCosts>();
+
+            foreach (var s in txtData.Skip(1))
+            {
+                var x = s.Split(' ');
+                inputData.Add(new EdgeCosts { V1 = Convert.ToInt32(x[0]), V2 = Convert.ToInt32(x[1]), Cost = Convert.ToInt32(x[1]) });
+            }
+            return inputData.OrderBy(x => x.Cost).ToList();
+        }
+
     }
 }
---
+++ SharedUtilities/EdgeCosts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedUtilities
+{
+    public class EdgeCosts
+    {
+        public int V1 { get; set; }
+        public int V2 { get; set; }
+        public int Cost { get; set; }
+    }
+}
